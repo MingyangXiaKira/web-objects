fdd488f361a4ea34868e2ed4da22fd60bd65996d
Mingyang <141198127+MingyangXiaKira@users.noreply.github.com>
2023-11-07 10:49:20 +0000

    Update App.jsx

--- ThreeJs/Objects1/src/App.jsx
+++ ThreeJs/Objects1/src/App.jsx
@@ -1,10 +1,14 @@
-function App() {
+import { createRoot } from "react-dom/client";
+import React, { useRef, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 
-  return (
-    <div>
-      Hello world!
-    </div>
-  )
+function Box(props) {
+  // This reference will give us direct access to the mesh
+  const meshRef = useRef();
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState();
+  const [clicked, setClick] = useState();
+  return <div></div>;
 }
 
-export default App
+export default App;
